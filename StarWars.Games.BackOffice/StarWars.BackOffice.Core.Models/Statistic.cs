4b9a7f5ebf8e5593c37fa0bcd1ecac01c9421a6d
Evan BOISSONNOT <evan@boissonnot.fr>
2024-01-16 10:44:57 +0100

    feat: debut journée 02

--- StarWars.Games.BackOffice/StarWars.BackOffice.Core.Models/Statistic.cs
+++ StarWars.Games.BackOffice/StarWars.BackOffice.Core.Models/Statistic.cs
@@ -8,6 +8,9 @@
 		public int Mois { get; set; }
 
 		public List<Game> GameList { get; set; } = new List<Game>();
+
+		public IEnumerable<Game> GamesAsSuccess => this.GameList.Where(item => item.Etat == "Success"); // Mieux renvoyer dès que vous le vous pouvez un IEnumerable
+		public IEnumerable<Game> GamesAsFailed => this.GameList.Where(item => item.Etat == "Failed");
 		#endregion
 	}
 }
--- StarWars.Games.BackOffice/StarWars.Games.BackOffice.Web.UI/Pages/ListStatistic.cshtml.cs
+++ StarWars.Games.BackOffice/StarWars.Games.BackOffice.Web.UI/Pages/ListStatistic.cshtml.cs
@@ -9,6 +9,16 @@ namespace StarWars.Games.BackOffice.Web.UI.Pages
 		public void OnGet()
 		{
 			this.List = (new StatisticService()).GetAll();
+
+			//foreach (var item in this.List)
+			//{
+			//	var games = item.GamesAsSuccess; // Lazy : ça ne s'exécute pas 
+			//									 //var result = games.ToList();
+			//	foreach (var game in games.Where(item => item.Id == 1))
+			//	{
+
+			//	}
+			//}
 			//this.List = new List<Statistic>();
 			//this.List.Add(new Statistic());
 		}
