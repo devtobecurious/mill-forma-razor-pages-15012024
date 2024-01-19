a705aa1acbcb0b997f6b0ceb9033621f5f7e3254
Evan BOISSONNOT <evan@boissonnot.fr>
2024-01-19 17:20:50 +0100

    feat: procedure stockées

--- StarWars.Games.BackOffice/StarWars.BackOffice.Core.Services/SqlServerDbContextGameService.cs
+++ StarWars.Games.BackOffice/StarWars.BackOffice.Core.Services/SqlServerDbContextGameService.cs
@@ -4,32 +4,34 @@ using StarWars.Games.Core.Interfaces;
 
 namespace StarWars.BackOffice.Core.Services
 {
-    public class SqlServerDbContextGameService : IGameService
-    {
-        private readonly DefaultDbContext context;
+	public class SqlServerDbContextGameService : IGameService
+	{
+		private readonly DefaultDbContext context;
 
-        public SqlServerDbContextGameService(DefaultDbContext context)
-        {
-            this.context = context;
-        }
+		public SqlServerDbContextGameService(DefaultDbContext context)
+		{
+			this.context = context;
+		}
 
-        public void Add(Game item)
-        {
-            this.context.Games.Add(item);
+		public void Add(Game item)
+		{
+			this.context.Games.Add(item);
 
-            //this.context.Games.Remove(item);
+			//this.context.Games.Remove(item);
 
-            this.context.SaveChanges();
-        }
+			this.context.SaveChanges();
+		}
 
-        public List<Game> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+		public List<Game> GetAll()
+		{
+			// this.context.Games.FromSqlRaw("dbo.maProcedureStokee", new { param1 = 1 }).ToList();
 
-        public List<Game> GetAll(Filter filter)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			return new List<Game>();
+		}
+
+		public List<Game> GetAll(Filter filter)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
